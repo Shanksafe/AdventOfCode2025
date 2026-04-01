2e021247107d5fa425f6a8df606b4fea843b91f8
Joseph Yi <yijose@myvuw.ac.nz>
2026-04-01 23:52:23 +1300

    Day 2 Test Code

--- src/main/kotlin/day02/Main.kt
+++ src/main/kotlin/day02/Main.kt
@@ -1,12 +1,9 @@
 package day02
 
 fun main() {
-    val input = object {}.javaClass
-        .getResource("/day02/input.txt")
-        ?.readText()
-        ?: error("File not found")
+    val input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
-    println(input)
+    val ranges = input.split(",")
 
-
+    println(ranges)
 }
